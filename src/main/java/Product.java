eb4d396b3c84208454821eb000b25f1e30a1a22c
sabuharb <abuharb.sandy@gmail.com>
2024-12-10 10:22:58 -0600

    Initial Commit

---
+++ src/main/java/Product.java
@@ -0,0 +1,57 @@
+public class Product {
+    private int productId;
+    private String name;
+    private String category;
+    private double price;
+
+    // Constructor
+    public Product(int productId, String name, String category, double price) {
+        this.productId = productId;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    // Getters and setters
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
